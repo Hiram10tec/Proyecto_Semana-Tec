c8e2e1178f590b07af10fcab0e42c27dbe3f79f4
Horizon899 <gaelcasares899@gmail.com>
2024-09-13 21:08:03 -0600

    Fix code to standard PEP 8

--- memory_game.py
+++ memory_game.py
@@ -1,21 +1,25 @@
 from random import shuffle
-from turtle import up, goto, down, color, begin_fill, forward, left, end_fill, clear
-from turtle import shape, stamp, write, update, ontimer, setup, addshape, hideturtle, tracer, onscreenclick, done
+from turtle import up, goto, down, color, begin_fill, forward, left, \
+    end_fill, clear
+from turtle import shape, stamp, write, update, ontimer, setup, addshape, \
+    hideturtle, tracer, onscreenclick, done
 from freegames import path
 
-#Imagen del carro
+
+# Imagen del carro
 car = path('car.gif')
 
-#Inicialización del juego
+# Inicialización del juego
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 
-#Contador de taps
-taps=0
+# Contador de taps
+taps = 0
+
 
 def square(x, y):
-    # Dibuja un cuadrado blanco en (x, y)
+    """Dibuja un cuadrado blanco en (x, y)."""
     up()
     goto(x, y)
     down()
@@ -28,24 +32,22 @@ def square(x, y):
 
 
 def index(x, y):
-    # Convierte las coordenadas (x, y) en un indice de tile
+    """Convierte las coordenadas (x, y) en un índice de tile."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
 
 def xy(count):
-    #Convierte el dinde de tile a coordenadas (x, y)
+    """Convierte el índice de tile a coordenadas (x, y)."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 
 def tap(x, y):
-    # Actualiza el estado de las marcas y tiles ocultos basados en el tap
-
-    # Saca la variable taps del local"
+    """Actualiza el estado de las marcas y tiles ocultos basados en el tap."""
     global taps
-    #Agrega 1 por cada tap"
-    taps= taps+1
+    taps += 1  # Incrementa taps en 1 por cada tap
     spot = index(x, y)
     mark = state['mark']
+
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
     else:
@@ -55,11 +57,12 @@ def tap(x, y):
 
 
 def draw():
-    #Dibuja la imagen, tiles y cuenta el numero de taps
+    """Dibuja la imagen, tiles y cuenta el número de taps."""
     clear()
     goto(0, 0)
     shape(car)
     stamp()
+
     # Dibuja el grid de tiles
     for count in range(64):
         if hide[count]:
@@ -68,28 +71,30 @@ def draw():
 
     mark = state['mark']
 
-    # Dibuja el numero en el tile marcado
+    # Dibuja el número en el tile marcado
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
         goto(x + 2, y)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
-    #Muestra los taps
+
+    # Muestra los taps
     up()
     goto(-200, -250)
     color("black")
     write(f'Taps: {taps}')
 
-    #Detecta si todos los cuadros han sido destapados
+    # Detecta si todos los cuadros han sido destapados
     if True not in hide:
-        goto(-50,0)
-        write("Lo lograste!")
+        goto(-50, 0)
+        write("¡Lo lograste!", font=('Arial', 20, 'normal'))
 
     update()
     ontimer(draw, 100)
 
-#Config del juego
+
+# Configuración del juego
 shuffle(tiles)
 setup(420, 420, 370, 0)
 addshape(car)
